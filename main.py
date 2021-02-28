d3242088f59860fe7829e9e426b8cfc2d2b63b86
RBmint <mzhai4@illinois.edu>
2021-02-28 17:39:17 -0600

    added import and export function

--- main.py
+++ main.py
@@ -2,7 +2,26 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from bson.json_util import dumps
 
+client = MongoClient("localhost:27017")
+db = client['mydb']
+book_collection = db['book']
+#book_collection.drop()
+author_collection = db['author']
+author_collection.drop()
+
+cursor = book_collection.find({})
+type_documents_count = db['book'].estimated_document_count()
+
+with open('ok.json', 'w') as file:
+    file.write('[')
+    for i, document in enumerate(cursor, 1):
+        file.write(dumps(document,indent = 4))
+        if i != type_documents_count:
+            file.write(',')
+    file.write(']')
 
 
 FIRSTBOOKURL = input("please enter the URL of first book\n")
@@ -12,12 +31,13 @@ try:
     print("URL is valid")
 except:
     print("URL has been automatically set")
+    #FIRSTBOOKURL = "https://www.goodreads.com/book/show/4099.The_Pragmatic_Programmer"
     FIRSTBOOKURL = "https://www.goodreads.com/book/show/3735293-clean-code"
 
 
 # Clear the JSON file each time we run this main.py
-open('Books.json', 'w').close()
-open('Authors.json', 'w').close()
+#open('Books.json', 'w').close()
+#open('Authors.json', 'w').close()
 
 # Converts the book_soup as well as the author_soup to a dictionary
 # that is easily converted to a JSON file.
@@ -102,7 +122,7 @@ author_dic = {}
 # unique books inside the book_dic.
 while True:
     # Scrap 10 similar books/authors at a time
-    for i in range (0, 21, 2):
+    for i in range (0, 11, 2):
         book_soup = BeautifulSoup(requests.get("https://www.goodreads.com" \
             + similar_books_soup.findAll(\
             "a", itemprop = "url", limit = 22)[i]['href']).content, "lxml")
@@ -110,14 +130,19 @@ while True:
         author_soup = BeautifulSoup(requests.get(\
         similar_books_soup.findAll(\
             "a", itemprop = "url", limit = 22)[i+1]['href']).content, "lxml")
-        book = SoupToJSON(book_soup, author_soup)
+        try:
+            book = SoupToJSON(book_soup, author_soup)
+        except:
+            continue        
         # Check if book/author is going to be a duplicate
         if book.book_info['title'] not in book_dic:
+            book_collection.insert_one(book.book_info)
             book_dic[book.book_info['title']] = book
         if book.author_info['author_id'] not in author_dic:
+            author_collection.insert_one(book.author_info)
             author_dic[book.author_info['author_id']] = book
     # The number of total books scraped
-    if len(book_dic) >= 10:
+    if len(book_dic) >= 5:
         break
     # Find similar book based on the last book stored in dictionary
     similar_books_soup = BeautifulSoup(\
@@ -127,11 +152,34 @@ while True:
 bookJSON = [book.book_info for book in book_dic.values()]
 authorJSON = [book.author_info for book in author_dic.values()]
 
-with open("Books.json", "a") as fp:
-    json.dump(bookJSON, fp, indent = 4)
-with open("Authors.json", "a") as fp:
-    json.dump(authorJSON, fp, indent = 4)
+# with open("Books.json", "a") as fp:
+#     json.dump(bookJSON, fp, indent = 4)
+# with open("Authors.json", "a") as fp:
+#     json.dump(authorJSON, fp, indent = 4)
 
 # Load the JSON file. Don't know how should we use this yet.
 # with open("Books.json", "r") as read_file:
 #     data = json.load(read_file)
+
+# with open("Books.json", encoding="utf-8") as jf:
+#     str = jf.read()
+#     data = []
+#     data.extend(json.loads(str))
+#     collection.insert_many(data)
+cursor = book_collection.find({})
+type_documents = db['cluster'].find()
+type_documents_count = db['book'].count_documents()
+
+with open('book.json', 'w') as file:
+    file.dumps(cursor, indent = 4)
+    file.write('[')
+    for i, document in enumerate(type_documents, 1):
+        file.write(dumps(document,indent = 4))
+        if i != type_documents_count:
+            file.write(',')
+    file.write(']')
+
+cursor = author_collection.find({})
+for document in cursor:
+    print(document["name"])
+
