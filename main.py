4756988029f72a7c5c88bcb5feb348a91043ad95
RBmint <mzhai4@illinois.edu>
2021-02-28 21:38:06 -0600

    addressed pylint issues

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-""" This main runs a web scraper and collect 200 unique books into a JSON file"""
+""" This runs a web scraper and can import/export JSON file to/from mongoDB"""
 import json
-import requests
 import argparse
+import requests
 
-from bs4 import BeautifulSoup
-from pymongo import MongoClient
-from bson.json_util import dumps
+from bs4 import BeautifulSoup #pylint: disable=E0401 
+from pymongo import MongoClient #pylint: disable=E0401 
+from bson.json_util import dumps #pylint: disable=E0401 
 
-
-def ExportBooksToJSON (file_name):
+def export_books_to_json (file_name):
+    """ Export the book database to a JSON file"""
     client = MongoClient("localhost:27017")
-    db = client['mydb']
-    book_collection = db['book']
+    database = client['mydb']
+    book_collection = database['book']
     cursor = book_collection.find({})
-    type_documents_count = db['book'].estimated_document_count()
+    type_documents_count = database['book'].estimated_document_count()
     with open(file_name, 'w') as file:
         file.write('[')
         for i, document in enumerate(cursor, 1):
@@ -23,12 +23,13 @@ def ExportBooksToJSON (file_name):
         file.write(']')
     file.close()
 
-def ExportAuthorsToJSON (file_name):
+def export_authors_to_json (file_name):
+    """ Export the author database to a JSON file"""
     client = MongoClient("localhost:27017")
-    db = client['mydb']
-    book_collection = db['author']
+    database = client['mydb']
+    book_collection = database['author']
     cursor = book_collection.find({})
-    type_documents_count = db['author'].estimated_document_count()
+    type_documents_count = database['author'].estimated_document_count()
     with open(file_name, 'w') as file:
         file.write('[')
         for i, document in enumerate(cursor, 1):
@@ -49,10 +50,10 @@ def ExportAuthorsToJSON (file_name):
 #     FIRSTBOOKURL = "https://www.goodreads.com/book/show/3735293-clean-code"
 
 
-# Converts the book_soup as well as the author_soup to a dictionary
-# that is easily converted to a JSON file.
 class SoupToJSON():
-    """This class stores the information from the scraper"""
+    """This class stores the information from the scraper.
+        Converts the book_soup as well as the author_soup to a dictionary
+        that is easily converted to a JSON file."""
     def __init__(self, book_soup_in, author_soup_in):
         self.book_info = {}
         self.book_info["book_url"] = \
@@ -96,10 +97,10 @@ class SoupToJSON():
         self.author_info["related_authors"] = \
             "https://www.goodreads.com" \
             + author_soup_in.find("a", text = "Similar authors")['href']
-        for url in author_soup_in.findAll("a", href = True):
-            if "author/list" in str(url):
+        for urls in author_soup_in.findAll("a", href = True):
+            if "author/list" in str(urls):
                 self.author_info["author_books"] = \
-                    "https://www.goodreads.com" + (url['href'])
+                    "https://www.goodreads.com" + (urls['href'])
             break
     def print_author_name(self):
         """Print the name of the author"""
@@ -109,13 +110,13 @@ class SoupToJSON():
         print(self.book_info["title"])
 
 
-# Scrap data from similar books URL until we have desired number of
-# unique books inside the book_dic.
-def StartScrapping(book_soup, book_count, author_count):
+def start_scraping(book_soup, book_count, author_count):
+    """ Scrap data from similar books URL until we have desired number of
+    unique books inside the book_dic."""
     # Get author URL manually
-    bookAuthorURL = book_soup.find("meta", property = "books:author")['content']
+    book_author_url = book_soup.find("meta", property = "books:author")['content']
     # Create author soup
-    author_soup = BeautifulSoup(requests.get(bookAuthorURL).content, "lxml")
+    author_soup = BeautifulSoup(requests.get(book_author_url).content, "lxml")
     # Create soupToBookJSON last
     book = SoupToJSON(book_soup, author_soup)
     # Get the soup of similar books
@@ -123,9 +124,9 @@ def StartScrapping(book_soup, book_count, author_count):
     book.book_info["similar_books"]).content, "lxml")
     # Connect to MongoDB
     client = MongoClient("localhost:27017")
-    db = client['mydb']
-    book_collection = db['book']
-    author_collection = db['author']
+    database = client['mydb']
+    book_collection = database['book']
+    author_collection = database['author']
     # Drop any collection that existed before
     book_collection.drop()
     author_collection.drop()
@@ -138,7 +139,7 @@ def StartScrapping(book_soup, book_count, author_count):
             book_soup = BeautifulSoup(requests.get("https://www.goodreads.com" \
                 + similar_books_soup.findAll(\
                 "a", itemprop = "url", limit = 22)[i]['href']).content, "lxml")
-            bookAuthorURL = book_soup.find("meta", property = "books:author")['content']
+            book_author_url = book_soup.find("meta", property = "books:author")['content']
             author_soup = BeautifulSoup(requests.get(\
             similar_books_soup.findAll(\
                 "a", itemprop = "url", limit = 22)[i+1]['href']).content, "lxml")
@@ -146,7 +147,7 @@ def StartScrapping(book_soup, book_count, author_count):
             try:
                 book = SoupToJSON(book_soup, author_soup)
             except:
-                continue        
+                continue
             # Check if book/author is going to be a duplicate
             if book.book_info['title'] not in book_dic:
                 book_collection.insert_one(book.book_info)
@@ -161,24 +162,27 @@ def StartScrapping(book_soup, book_count, author_count):
         similar_books_soup = BeautifulSoup(\
             requests.get(book.book_info["similar_books"]).content, "lxml")
 
-def ReadFromBookJSON(file_to_read_from):
+def read_from_book_json(file_to_read_from):
+    """ Read from a JSON file containing books and add to database"""
     client = MongoClient("localhost:27017")
-    db = client['mydb']
-    book_collection = db['book']
-    with open(file_to_read_from, encoding="utf-8") as f:
-        str = f.read()
+    database = client['mydb']
+    book_collection = database['book']
+    with open(file_to_read_from, encoding="utf-8") as file:
+        to_load = file.read()
         data = []
-        data.extend(json.loads(str))
+        data.extend(json.loads(to_load))
         book_collection.insert_many(data)
 
-def ReadFromAuthorJSON(file_to_read_from):
+
+def read_from_author_json(file_to_read_from):
+    """ Read from a JSON file containing authors and add to database"""
     client = MongoClient("localhost:27017")
-    db = client['mydb']
-    author_collection = db['author']
-    with open(file_to_read_from, encoding="utf-8") as f:
-        str = f.read()
+    database = client['mydb']
+    author_collection = database['author']
+    with open(file_to_read_from, encoding="utf-8") as file:
+        to_load = file.read()
         data = []
-        data.extend(json.loads(str))
+        data.extend(json.loads(to_load))
         author_collection.insert_many(data)
 
 
@@ -197,12 +201,12 @@ args = parser.parse_args()
 
 if args.readbook:
     try:
-        ReadFromBookJSON(args.readbook)
+        read_from_book_json(args.readbook)
     except:
         print("JSON file is invalid")
 if args.readauthor:
     try:
-        ReadFromAuthorJSON(args.readauthor)
+        read_from_author_json(args.readauthor)
     except:
         print("JSON file is invalid")
 if args.author > 50:
@@ -212,15 +216,15 @@ if args.book > 200:
 if args.url:
     try:
         url = requests.get(args.url)
-        book_soup = BeautifulSoup(requests.get(args.url).content, "lxml")
-        book_id = book_soup.find("input", id = "book_id")['value']
-        if (book_id != 0):
+        valid_book_soup = BeautifulSoup(requests.get(args.url).content, "lxml")
+        book_id = valid_book_soup.find("input", id = "book_id")['value']
+        if book_id != 0:
             print("URL is valid")
-            StartScrapping(book_soup, args.book, args.author)
+            start_scraping(valid_book_soup, args.book, args.author)
     except:
         print("URL is invalid")
-if args.exbook:
-    ExportBooksToJSON(args.exbook)
-if args.exauthor:
-    ExportAuthorsToJSON(args.exauthor)
 
+if args.exbook:
+    export_books_to_json(args.exbook)
+if args.exauthor:
+    export_authors_to_json(args.exauthor)
